6ff2a12bd657e087928ee17b5cd4de83d21c85e4
Aleksandr <aleksandrnosov2008@gmail.com>
2024-02-12 22:25:10 +0300

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length 
+  && arr1.every((element, index) => element === arr2[index])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if(users.length === 0 || !users || !Array.isArray(users) ||
+     typeof gender !== "string" || !gender ) {
+    return 0;
+  }
+  let filterUser = users.filter(user => user.gender === gender);
+  if(filterUser.length === 0) {
+    return 0;
+  }
+  let totalAge = filterUser.reduce((acc, user) => acc + user.age, 0);
+  let averageAge = totalAge / filterUser.length;
+  return averageAge;
 }
